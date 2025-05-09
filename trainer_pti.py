478163f836c1166b98da35383d599171a892c2ac
DanhVinhLe <132262572+DanhVinhLe@users.noreply.github.com>
2025-05-09 15:04:12 +0700

    fix code

--- trainer_pti.py
+++ trainer_pti.py
@@ -370,7 +370,7 @@ def main(
                     )
 
                 else:
-                    lora_tensors = unet_attn_processors_state_dict(unet)
+                    lora_tensors = unet_attn_processors_state_dict(peft_unet)
 
                     save_file(
                         lora_tensors,
@@ -394,7 +394,7 @@ def main(
             f"{output_dir}/unet.safetensors",
         )
     else:
-        lora_tensors = unet_attn_processors_state_dict(unet)
+        lora_tensors = unet_attn_processors_state_dict(peft_unet)
         save_file(
             lora_tensors,
             f"{output_dir}/lora.safetensors",
